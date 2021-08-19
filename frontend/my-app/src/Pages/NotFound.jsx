5436ca562ffb1ea96a4967bf630f5d7809c976c9
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-08-20 03:17:46 +0900

    Fix savedata

--- frontend/my-app/src/Pages/NotFound.jsx
+++ frontend/my-app/src/Pages/NotFound.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Center, Text } from "@chakra-ui/react";
+import { AiOutlineCaretRight } from "react-icons/ai";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const NotFound = () => {
   return (
     <div>
-      <Center mt={30}>
+      <Header />
+      <Center mt={70}>
         <Center bg="teal" h="200px" w="400px" color="white" borderRadius="lg">
           <Text fontSize="4xl">404 NotFound</Text>
         </Center>
       </Center>
-      <Center mt={30}>
-        <Link to="/">TOPに戻る →</Link>
+      <Center mt={30} mb={20}>
+        <Link to="/">
+          TOPページへ{" "}
+          <AiOutlineCaretRight
+            style={{ display: "inline-flex", verticalAlign: "middle" }}
+          />
+        </Link>
       </Center>
+      <Footer />
     </div>
   );
 };
